788ba0618cdb394131b70c570f09f93cf3263d96
redgoblin88 <t.harper12345@gmail.com>
2018-02-04 08:25:16 +0000

    Create Gsearch.js

---
+++ Gsearch.js
@@ -0,0 +1,10 @@
+(function() {
+				var cx = '009969993746896670841:byot1a-991i';
+				var gcse = document.createElement('script');
+				var googleNumSearchResults = 5;
+				gcse.type = 'text/javascript';
+				gcse.async = true;
+				gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//cse.google.com/cse.js?cx=' + cx;
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(gcse, s);
+			})();
